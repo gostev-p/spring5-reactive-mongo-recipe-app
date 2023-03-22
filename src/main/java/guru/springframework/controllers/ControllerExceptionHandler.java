e9dc2d9697f29d2ac309c3cacd2d66bd563c8a5a
Gostev, Philipp <philipp.gostev@adesso.de>
2023-03-22 11:23:46 +0100

    refactor to show error pages

--- src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.support.WebExchangeBindException;
-import org.thymeleaf.exceptions.TemplateInputException;
 //import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -21,7 +20,7 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(WebExchangeBindException.class)
     public String handleNumberFormat(Exception exception, Model model){
 
-        log.error("Handling Number Format Exception");
+        log.error("Handling Binding Exception");
         log.error(exception.getMessage());
 
         model.addAttribute("exception", exception);
